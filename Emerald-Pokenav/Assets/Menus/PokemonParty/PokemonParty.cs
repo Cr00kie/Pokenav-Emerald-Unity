f530b1fec030bec66f1fe03a9f0de5e66f700e02
Adrian Isasi <aisasi505@gmail.com>
2026-04-18 21:17:45 +0200

    Read pokemon database from a json file

--- Emerald-Pokenav/Assets/Menus/PokemonParty/PokemonParty.cs
+++ Emerald-Pokenav/Assets/Menus/PokemonParty/PokemonParty.cs
@@ -1,5 +1,8 @@
-    using System.Collections;
+using JetBrains.Annotations;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -23,10 +26,8 @@ public class PokemonParty : MonoBehaviour
 
     private void OnEnable()
     {
-
-        pkmnDatabase = new Dictionary<string, Pokemon>();
-        populateDatabase();
-
+        populateDatabaseFromJSONFile("pokemonDatabase.json");
+        
         UIDocument document = GetComponent<UIDocument>();
         root = document.rootVisualElement;
 
@@ -112,36 +113,43 @@ public class PokemonParty : MonoBehaviour
         changeSelectedPokemon(newPokemonID);
     }
 
-    // Crea una base de datos para testear
-    void populateDatabase()
+    void populateDatabaseFromJSONFile(string jsonFile)
     {
-        pkmnDatabase.Add("rayquaza", new Pokemon("Rayquaza", 75, "rayquaza", 0.7f, 0.10f, 0.2f, 0.6f, 0.8f));
-        pkmnDatabase.Add("pidgeot", new Pokemon("Pidgeot", 25, "pidgeot", 0.3f, 0.12f, 0.3f, 0.4f, 0.1f));
-        pkmnDatabase.Add("gligar", new Pokemon("Gligar", 23, "gligar", 0.2f, 0.6f, 0.8f, 0.1f, 0.5f));
-        pkmnDatabase.Add("treecko", new Pokemon("Treecko", 34, "treecko", 0.8f, 0.11f, 0.1f, 0.4f, 0.8f));
-        pkmnDatabase.Add("poke4", new Pokemon("Pokemon 4", 76, "", 0.1f, 0.13f, 0.5f, 0.3f, 0.5f));
-        pkmnDatabase.Add("poke5", new Pokemon("Pokemon 5", 12, "", 0.4f, 0.2f, 0.8f, 0.7f, 0.4f));
-        pkmnDatabase.Add("poke6", new Pokemon("Pokemon 6", 45, "", 0.2f, 0.6f, 0.1f, 0.1f, 0.3f));
-        pkmnDatabase.Add("poke7", new Pokemon("Pokemon 7", 23, "", 0.9f, 0.10f, 0.2f, 0.4f, 0.9f));
+        pkmnDatabase = new Dictionary<string, Pokemon>();
+
+        PokemonList pokemons = JsonUtility.FromJson<PokemonList>(File.ReadAllText(jsonFile));
+        foreach (var pokemon in pokemons.pokemons)
+        {
+            pkmnDatabase.Add(pokemon.key, pokemon); // use key field
+        }
     }
 
     Dictionary<string, Pokemon> pkmnDatabase;
 }
 
+[Serializable] class PokemonList
+{
+    public List<Pokemon> pokemons;
+}
+
+[Serializable]
 public struct Pokemon
 {
-    public Pokemon(string name, int level, string imgFileName, float cool, float tough, float beauty, float smart, float cute)
+    public Pokemon(string key, string name, int level, string imgFileName, float cool, float tough, float beauty, float smart, float cute)
     {
+        this.key = key;
         this.name = name;
         this.level = level;
         this.imgFileName = imgFileName;
         stats = new PokemonStats(cool, tough, beauty, smart, cute);
     }
+    public string key;
     public string name;
     public int level;
     public string imgFileName;
     public PokemonStats stats;
 
+    [Serializable]
     public struct PokemonStats{
         public PokemonStats(float cool, float tough, float beauty, float smart, float cute)
         {
